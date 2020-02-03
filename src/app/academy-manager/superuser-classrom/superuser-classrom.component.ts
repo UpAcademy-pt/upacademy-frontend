3874a202cabf8c467787b233911829787b62efd9
CristianoVale <cristiano.fcvale@gmail.com>
2020-02-03 02:07:03 +0000

    Fixed profile. Added Classrom

--- src/app/academy-manager/superuser-classrom/superuser-classrom.component.ts
+++ src/app/academy-manager/superuser-classrom/superuser-classrom.component.ts
@@ -8,6 +8,8 @@ import { AcademyService } from '../shared/services/academy.service';
 import { Academy } from '../shared/models/academy';
 import { UserServiceService } from 'src/app/core/services/user-service/user-service.service';
 import { User } from 'src/app/core/models/user';
+import { MissedclassesService } from '../shared/services/missedclasses.service';
+import { Missed } from '../shared/models/missed';
 
 @Component({
   selector: 'app-superuser-classrom',
@@ -15,6 +17,8 @@ import { User } from 'src/app/core/models/user';
   styleUrls: ['./superuser-classrom.component.scss']
 })
 export class SuperuserClassromComponent implements OnInit {
+  public view = false;
+
   public tempAccount: Account;
   public tempAccount$: ReplaySubject<Account> = new ReplaySubject(1);
 
@@ -31,24 +35,26 @@ export class SuperuserClassromComponent implements OnInit {
   public arrayPositions: any[] = [];
   public arrayPositions$: ReplaySubject<any[]> = new ReplaySubject(1);
 
-  public tempPosAcc = new Posbyaccount();
-  public tempPosAcc2 = new Posbyaccount();
+   public missedClassArray: number[] = [];
+
+  // public tempPosAcc = new Posbyaccount();
+  // public tempPosAcc2 = new Posbyaccount();
 
   constructor(
-    private renderer: Renderer2,
     private accountApi: AccountService,
     private academyApi: AcademyService,
     private userApi: UserServiceService,
+    private missedApi: MissedclassesService,
     private http: HttpClient) {
     this.getAllStudents();
-    this.getUserAccount();
-    console.log(this.arrayPositions);
-    this.arrayPositions.map(pos => {
-      pos.url = this.getUserUrl();
-      pos.name = this.getUserName();
-    });
-    this.arrayPositions$.next(this.arrayPositions)
-    console.log(this.tempAccount);
+    // this.getUserAccount();
+    // console.log(this.arrayPositions);
+    // this.arrayPositions.map(pos => {
+    //   pos.url = this.getUserUrl();
+    //   pos.name = this.getUserName();
+    // });
+    // this.arrayPositions$.next(this.arrayPositions)
+    // console.log(this.tempAccount);
   }
 
   ngOnInit() {
@@ -77,29 +83,42 @@ public getUser(account: Account){
 });
 }
 
-
-
-  public getUserUrl() {
-    this.accountApi.getById(2).subscribe((data: any) => {
-      this.tempAccount = data;
-      this.getUserName();
-    });
-  }
-  public getUserAccount() {
-  }
-
-  getUserName() {
-    return "Bruno";
-  }
-
-  addRows() {
-  }
-
-  setphoto() {
-  }
-
   public cleanElement() {
   }
 
+  public addMissed(id: number){
+    var timeInMs = Date.now();
+    var date = new Date(timeInMs);
+    var year = date.getFullYear();
+    var month = ("0" + (date.getMonth() + 1)).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+
+    let missedClass = new Missed;
+    missedClass.data = "" + year+"-"+month+"-"+day;
+    missedClass.accountId = id;
+    missedClass.justified = false;
+
+    this.missedApi.create(missedClass).subscribe(
+      (res: any) =>  console.log(res))
+
+    this.missedClassArray.push(id);
+  }
+
+  public removeMissed(id: number){
+
+  }
+
+  public chekedIfMissed(id: number){
+    return this.missedClassArray.includes(id);
+  }
+
+  public changeView(){
+    if (this.view){
+      this.view = false;
+    }
+    else {
+      this.view = true;
+    }
+  }
 }
 
